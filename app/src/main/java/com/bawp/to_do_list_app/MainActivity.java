bf2f775196706969fe3bdff38434928c845c4c14
Anna Rabynovych <rabynan@ac.sce.ac.il>
2020-12-20 11:15:08 +0200

    Step #3
    Addind current date and changing color of the title bar of the application and also moving automatically from one activity to anoter.

--- app/src/main/java/com/bawp/to_do_list_app/MainActivity.java
+++ app/src/main/java/com/bawp/to_do_list_app/MainActivity.java
@@ -1,14 +1,35 @@
 package com.bawp.to_do_list_app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doStuff();
+            }
+        },5000);
+    }
+    private void doStuff(){
+        Intent intent = new Intent(MainActivity.this,Screen.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/bawp/to_do_list_app/Screen.java
+++ app/src/main/java/com/bawp/to_do_list_app/Screen.java
@@ -1,8 +1,15 @@
 package com.bawp.to_do_list_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Screen extends AppCompatActivity {
 
@@ -10,5 +17,9 @@ public class Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
+        TextView textView=findViewById(R.id.date);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd ");
+        String currentDateandTime = sdf.format(new Date());
+        textView.setText(currentDateandTime);
     }
 }
